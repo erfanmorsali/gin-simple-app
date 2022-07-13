ac0d334ae8f4218361a52f424dca35a434372077
erfanmorsali <erfanmorsalidev@gmail.com>
2022-07-13 14:35:16 +0430

    Add GetById And DeleteUserById To UserController

--- users/controllers/user_controller.go
+++ users/controllers/user_controller.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"github.com/erfanmorsali/gin-simple-app.git/users/interfaces"
+	"github.com/erfanmorsali/gin-simple-app.git/users/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type UserController struct {
@@ -17,4 +19,63 @@ func NewUserController(userService interfaces.UserService) *UserController {
 func (c UserController) GetAllUsers(context *gin.Context) {
 	context.JSON(http.StatusOK, c.UserService.GetAllUsers())
 	return
+}
+
+func (c UserController) CreateUser(context *gin.Context) {
+	var userIn models.UserIn
+
+	err := context.ShouldBindJSON(&userIn)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	userOut := c.UserService.CreateUser(userIn)
+	context.JSON(http.StatusOK, userOut)
+	return
+}
+
+func (c UserController) GetUserById(context *gin.Context) {
+	pathId := context.Param("id")
+	id, err := strconv.Atoi(pathId)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid input for id",
+		})
+		return
+	}
+
+	userOut, err := c.UserService.GetUserById(uint(id))
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, userOut)
+	return
+}
+
+func (c UserController) DeleteUserById(context *gin.Context) {
+	pathId := context.Param("id")
+	id, err := strconv.Atoi(pathId)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = c.UserService.DeleteUserById(uint(id))
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, true)
 }
