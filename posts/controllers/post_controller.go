7ca3b4f581efbc3c68b9df3f8b56be1440e95dc2
e.morsali <erfanmorsalidev@gmail.com>
2022-07-14 21:01:27 +0430

    Create PostController

---
+++ posts/controllers/post_controller.go
@@ -0,0 +1,96 @@
+package controllers
+
+import (
+	"github.com/erfanmorsali/gin-simple-app.git/posts/dtos"
+	"github.com/erfanmorsali/gin-simple-app.git/posts/interfaces"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type PostController struct {
+	PostService interfaces.PostService
+}
+
+func NewPostController(service interfaces.PostService) *PostController {
+	return &PostController{PostService: service}
+}
+
+func (c PostController) GetAllPosts(context *gin.Context) {
+	posts, err := c.PostService.GetAllPosts()
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, posts)
+	return
+}
+
+func (c PostController) GetPostById(context *gin.Context) {
+	param := context.Param("id")
+	id, err := strconv.Atoi(param)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	postOut, err := c.PostService.GetPostById(uint(id))
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, postOut)
+	return
+}
+
+func (c PostController) CreatePost(context *gin.Context) {
+	var postIn dtos.PostIn
+	err := context.ShouldBindJSON(&postIn)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	post, err := c.PostService.CreatePost(postIn)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	context.JSON(http.StatusCreated, post)
+	return
+}
+
+func (c PostController) DeletePostById(context *gin.Context) {
+	param := context.Param("id")
+	id, err := strconv.Atoi(param)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = c.PostService.DeletePostById(uint(id))
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, true)
+	return
+}
