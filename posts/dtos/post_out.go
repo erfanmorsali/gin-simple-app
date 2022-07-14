36f971ce93dc135848456599d52f28f5371d3856
e.morsali <erfanmorsalidev@gmail.com>
2022-07-14 21:05:14 +0430

    Add CategoryInfos To PostOut

--- posts/dtos/post_out.go
+++ posts/dtos/post_out.go
@@ -1,10 +1,32 @@
 package dtos
 
-import "github.com/erfanmorsali/gin-simple-app.git/users/dtos"
+import (
+	"github.com/erfanmorsali/gin-simple-app.git/database/models"
+)
 
 type PostOut struct {
-	ID          uint          `json:"id"`
-	Title       string        `json:"title"`
-	Description string        `json:"description"`
-	UserInfo    dtos.UserInfo `json:"user"`
+	ID            uint           `json:"id"`
+	Title         string         `json:"title"`
+	Description   string         `json:"description"`
+	UserInfo      *userInfo      `json:"user"`
+	CategoryInfos []categoryInfo `json:"categories"`
+}
+
+func CreatePostOut(post models.Post) PostOut {
+	var categoryInfos []categoryInfo
+	var user *userInfo
+
+	if len(post.Categories) >= 1 {
+		for _, category := range post.Categories {
+			var categoryInfo = categoryInfo{ID: category.ID, Name: category.Name}
+			categoryInfos = append(categoryInfos, categoryInfo)
+		}
+
+	}
+
+	if post.User.ID != 0 {
+		user = &userInfo{ID: post.User.ID, Username: post.User.Username}
+	}
+
+	return PostOut{ID: post.ID, Title: post.Title, Description: post.Description, CategoryInfos: categoryInfos, UserInfo: user}
 }
