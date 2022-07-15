454db70c866c2f4425ec331dc162baf08105ab63
e.morsali <erfanmorsalidev@gmail.com>
2022-07-15 12:59:43 +0430

    Add New Methods To CategoryService Interface And Its Implementation

--- posts/interfaces/category_service.go
+++ posts/interfaces/category_service.go
@@ -1,4 +1,12 @@
 package interfaces
 
+import (
+	"github.com/erfanmorsali/gin-simple-app.git/posts/dtos"
+)
+
 type CategoryService interface {
+	GetAll() ([]dtos.CategoryOut, error)
+	GetById(id uint) (*dtos.CategoryOut, error)
+	Create(categoryIn dtos.CategoryIn) (*dtos.CategoryOut, error)
+	DeleteById(id uint) error
 }
--- posts/services/category_service.go
+++ posts/services/category_service.go
@@ -1,6 +1,10 @@
 package services
 
-import "github.com/erfanmorsali/gin-simple-app.git/posts/interfaces"
+import (
+	"github.com/erfanmorsali/gin-simple-app.git/database/models"
+	"github.com/erfanmorsali/gin-simple-app.git/posts/dtos"
+	"github.com/erfanmorsali/gin-simple-app.git/posts/interfaces"
+)
 
 type CategoryService struct {
 	CategoryDao interfaces.CategoryDao
@@ -8,4 +12,57 @@ type CategoryService struct {
 
 func NewCategoryService(categoryDao interfaces.CategoryDao) *CategoryService {
 	return &CategoryService{CategoryDao: categoryDao}
+}
+
+func (s CategoryService) GetAll() ([]dtos.CategoryOut, error) {
+	categories, err := s.CategoryDao.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var result []dtos.CategoryOut
+
+	for _, category := range categories {
+		categoryOut := dtos.CreateCategoryOut(category)
+		result = append(result, categoryOut)
+	}
+
+	return result, nil
+}
+
+func (s CategoryService) GetById(id uint) (*dtos.CategoryOut, error) {
+	category, err := s.CategoryDao.GetById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	result := dtos.CreateCategoryOut(*category)
+
+	return &result, nil
+}
+
+func (s CategoryService) Create(categoryIn dtos.CategoryIn) (*dtos.CategoryOut, error) {
+	category := models.Category{Name: categoryIn.Name}
+
+	createdCategory, err := s.CategoryDao.Create(category)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryOut := dtos.CreateCategoryOut(*createdCategory)
+	return &categoryOut, nil
+}
+
+func (s CategoryService) DeleteById(id uint) error {
+	category, err := s.CategoryDao.GetById(id)
+	if err != nil {
+		return err
+	}
+
+	err = s.CategoryDao.Delete(*category)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
