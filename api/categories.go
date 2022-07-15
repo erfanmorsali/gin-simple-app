b997c1905649bca5369395416b4bee764180a2c1
e.morsali <erfanmorsalidev@gmail.com>
2022-07-15 13:01:37 +0430

    Create CategoryGroup Api

---
+++ api/categories.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"github.com/erfanmorsali/gin-simple-app.git/posts/controllers"
+	"github.com/erfanmorsali/gin-simple-app.git/posts/repositories"
+	"github.com/erfanmorsali/gin-simple-app.git/posts/services"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func RunCategoriesApi(engine *gin.Engine, db *gorm.DB) {
+	dao := repositories.NewCategoryDao(db)
+	service := services.NewCategoryService(dao)
+	controller := controllers.NewCategoryController(service)
+
+	categoryGroup := engine.Group("/categories")
+
+	categoryGroup.GET("/", controller.GetAllCategories)
+	categoryGroup.GET("/:id", controller.GetCategoryById)
+	categoryGroup.POST("/", controller.CreateCategory)
+	categoryGroup.DELETE("/:id", controller.DeleteCategoryById)
+}
