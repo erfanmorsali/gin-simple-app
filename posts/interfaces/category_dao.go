a8f9ccced44297ab0c5fde1e519adfba890460f8
e.morsali <erfanmorsalidev@gmail.com>
2022-07-15 12:59:17 +0430

    Add New Methods To CategoryDao Interface And Its Implementation

--- posts/interfaces/category_dao.go
+++ posts/interfaces/category_dao.go
@@ -4,4 +4,8 @@ import "github.com/erfanmorsali/gin-simple-app.git/database/models"
 
 type CategoryDao interface {
 	GetByIds(ids []uint) ([]models.Category, error)
+	GetAll() ([]models.Category, error)
+	GetById(id uint) (*models.Category, error)
+	Create(category models.Category) (*models.Category, error)
+	Delete(category models.Category) error
 }
--- posts/repositories/category_dao.go
+++ posts/repositories/category_dao.go
@@ -23,6 +23,38 @@ func (d CategoryDao) GetByIds(ids []uint) ([]models.Category, error) {
 	return categories, nil
 }
 
-func (d CategoryDao) GetById() {
+func (d CategoryDao) GetAll() ([]models.Category, error) {
+	var categories []models.Category
 
+	if err := d.Db.Find(&categories).Error; err != nil {
+		return nil, err
+	}
+
+	return categories, nil
+}
+
+func (d CategoryDao) GetById(id uint) (*models.Category, error) {
+	var category models.Category
+
+	if err := d.Db.First(&category, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &category, nil
+}
+
+func (d CategoryDao) Create(category models.Category) (*models.Category, error) {
+	if err := d.Db.Create(&category).Error; err != nil {
+		return nil, err
+	}
+
+	return &category, nil
+}
+
+func (d CategoryDao) Delete(category models.Category) error {
+	if err := d.Db.Delete(&category).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
